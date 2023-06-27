0e014fb6462b21eb36c79ed1d3a06b454dec04e1
Gurjant Singh <gurjantrandhawa74456@gmail.com>
2023-06-27 18:45:44 +0530

    cars showcase

--- components/CustomFilter.tsx
+++ components/CustomFilter.tsx
@@ -1,7 +1,7 @@
 "use client"
 import {CustomFilterProps} from "../types";
-import {Listbox} from "@headlessui/react";
-import {useState} from "react";
+import {Listbox, Transition} from "@headlessui/react";
+import {Fragment, useState} from "react";
 import Image from "next/image";
 
 export default function CustomFilter({title, options}: CustomFilterProps) {
@@ -15,12 +15,22 @@ export default function CustomFilter({title, options}: CustomFilterProps) {
             <div className={"relative w-fit z-10"}>
                 <Listbox.Button className={"custom-filter__btn"}>
                     <span className={"block truncate"}>{selected.title}</span>
-                    <Image src={"/chevron-up-down.svg"} alt={""}
+                    <Image src={"/chevron-up-down.svg"} alt={"up-down-chevron"}
                            width={20}
                            height={20}
                            className={"object-contain"}
                     />
                 </Listbox.Button>
+                <Transition
+                    as={Fragment}
+                    Leave={"transition ease in duration-100"}
+                    Leavefrom={"opacity-100"}
+                    LeaveTo={"opacity-0"}
+                >
+<Listbox.Options>
+
+</Listbox.Options>
+                </Transition>
             </div>
         </Listbox>
     </div>
